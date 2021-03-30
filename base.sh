fe448961b69a7625a1a816e018efab78505f5932
Ondřej Budai <ondrej@budai.cz>
2021-03-30 08:42:04 +0200

    shellcheck & cleanup

--- base.sh
+++ base.sh
@@ -1,28 +1,41 @@
 #!/bin/bash
+# shellcheck disable=SC2034
+# SC2034 - Ignore unused variables because this script is meant to be sourced
+#          into other scripts.
 
-set -x
+# This script is included in all stages - prepare, exec and cleanup.
+# It defines basic variables and functions.
 
-BASE="/home/$(whoami)"
-TERRAFORM_JOBS="${BASE}/terraform-jobs"
-mkdir -p "${TERRAFORM_JOBS}"
+set -euo pipefail
 
-RUNNER_DIR="${TERRAFORM_JOBS}/${CUSTOM_ENV_RUNNER}"
+# create directory for jobs
+JOBS="/home/$(whoami)/jobs"
+mkdir -p "${JOBS}"
+
+# create directory the specified runner
+RUNNER_DIR="${JOBS}/${CUSTOM_ENV_RUNNER}"
 mkdir -p "${RUNNER_DIR}"
 
-TERRAFORM_JOB="${RUNNER_DIR}/${CUSTOM_ENV_CI_JOB_ID}"
+# define a directory for this specific job
+JOB="${RUNNER_DIR}/${CUSTOM_ENV_CI_JOB_ID}"
 
-TERRAFORM="terraform -chdir=$TERRAFORM_JOB"
+# alias for launching terraform in the job's directory
+TERRAFORM="terraform -chdir=$JOB"
 
+# ServerAliveInterval helps with bad connectivity from/to the internal
+# VPC
 SSH="ssh -o ServerAliveInterval=60 -o StrictHostKeyChecking=no"
 
+# Helpers extracting values from the runner's config.json.
 function sshUser() {
-  cat "${TERRAFORM_JOB}/config.json" | jq -r '.user'
+  cat "${JOB}/config.json" | jq -r '.user'
 }
 
 function runnerArch() {
-  cat "${TERRAFORM_JOB}/config.json" | jq -r '.runnerArch'
+  cat "${JOB}/config.json" | jq -r '.runnerArch'
 }
 
+# Rename OpenStack authentication variables to the right names.
 set +x
 export OS_PROJECT_ID="${CUSTOM_ENV_OS_PROJECT_ID}"
 export OS_AUTH_URL="${CUSTOM_ENV_OS_AUTH_URL}"
