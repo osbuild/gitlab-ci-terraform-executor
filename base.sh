834b158ebaadc5dabb4723a801737d78f94c3b60
Ondřej Budai <ondrej@budai.cz>
2022-03-31 10:50:49 +0200

    base.sh: fix SSH in waitForUserLogout
    Previously the whole SSH command was passed as one string, that's not what
    we want. We actually want to pass the command as an array.

--- base.sh
+++ base.sh
@@ -31,7 +31,7 @@ function runnerArch() {
 function waitForUserLogout() {
     COMMAND="who -s | wc -l"
     VM_IP=$(cat "${JOB}/ip")
-    RESULT=$("$SSH" "$(sshUser)@${VM_IP}" "$COMMAND")
+    RESULT=$($SSH "$(sshUser)@${VM_IP}" "$COMMAND")
 	while (( "$RESULT" > 0 )); do
 		sleep 30
 	done
